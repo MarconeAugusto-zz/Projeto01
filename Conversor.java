1feb821fc9dc3fa2d5630a6f2443a19496eecdc0
Marcone Augusto <marcone.p.louzada@gmail.com>
2017-08-07 11:37:39 -0300

    Altereração na função

--- Conversor.java
+++ Conversor.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Conversor{
 
 	public static void ola(String nome){
@@ -6,11 +7,22 @@ public class Conversor{
 
 
 	public static void main(String args[]){
+		Scanner in = new Scanner(System.in);
+		double x = in.nextDouble();
+		System.out.println("Digite o valor em Km que deseja converter: ");
 		
 		System.out.println("Ola mundo");
 		ola("Maria");
 	}
+	
+	public static double KmMilha(double Km){
+
+		System.out.println("O valor em milhas é: ");
+	
+		return Km * 1.60934;
+	}
 }
+
 
 
 	
