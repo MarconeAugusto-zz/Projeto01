7d1d845515df7e16b4b4e388064c7d19df43eefd
Guilherme Roque <roqueee007@gmail.com>
2017-08-07 11:37:50 -0300

    CelsiusFah

--- Conversor.java
+++ Conversor.java
@@ -1,14 +1,24 @@
+import java.util.Scanner;
 public class Conversor{
 
 	public static void ola(String nome){
 		System.out.println("Bom dia " +nome);
 	}
 
-
+	public static double celsiusFahrenheit(double celsius){		
+		return celsius*1.8 +32;
+	
+	}
 	public static void main(String args[]){
 		
 		System.out.println("Ola mundo");
 		ola("Maria");
+
+		Scanner in = new Scanner(System.in);
+		double d = in.nextDouble();
+		System.out.println(celsiusFahrenheit(d));
+
+		
 	}
 }
 
